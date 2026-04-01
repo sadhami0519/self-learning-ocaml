404bdf766fe28ee6e2452ab6d17e17ff3eaa2820
Sakshi M <amisadha0519@gmail.com>
2026-04-01 22:29:09 +0530

    Add comments for readability later
    Implement raindrop function to convert numbers to raindrop sounds.

--- raindrops.ml
+++ raindrops.ml
@@ -1,1 +1,14 @@
+let raindrop _ =
+    failwith "'raindrop' is missing"
 
+let raindrop drop =
+let result = (
+(*The '^' operator concatenates the printed values*)
+  (if drop mod 3 = 0 then "Pling" else "") ^
+  (if drop mod 5 = 0 then "Plang" else "") ^
+  (if drop mod 7 = 0 then "Plong" else "")
+  )
+  in
+  (*if the above if conditions aren't satisfied, return input value as a string*)
+  if result = "" then string_of_int drop
+  else result
